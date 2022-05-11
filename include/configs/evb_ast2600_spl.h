e343837f55ca77f4e45dd398b7e92995c96bd810
Steven Lee <steven_lee@aspeedtech.com>
2022-05-11 11:12:55 +0800

    Support generating signed capsule for ast2600-pfr and
    %% original patch: 0001-change-boot-addr-dcscm.patch

--- include/configs/evb_ast2600_spl.h
+++ include/configs/evb_ast2600_spl.h
@@ -32,7 +32,7 @@
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
-	"bootspi=fdt addr 20100000 && fdt header get fitsize totalsize && cp.b 20100000 ${loadaddr} ${fitsize} && bootm; echo Error loading kernel FIT image\0"	\
+	"bootspi=fdt addr 21520000 && fdt header get fitsize totalsize && cp.b 21520000 ${loadaddr} ${fitsize} && bootm; echo Error loading kernel FIT image\0"	\
 	"verify=yes\0"	\
 	""
 
