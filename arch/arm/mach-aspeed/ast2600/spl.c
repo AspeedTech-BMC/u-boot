559d06643f9a5d4c5b0b0e70c27b942c9a9df707
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2023-07-20 09:47:18 +0800

    spl: ast2600: Add dynamic OPTEE NS entry support
    The generic spl_optee_entry() always takes SYS_TEXT_BASE
    as the NS entry for OPTEE. However, with the PIE and FIT
    supoort, U-Boot can be loaded and executed at an address
    other than the compiled SYS_TEXT_BASE.
    
    Accordingly, we should relax spl_optee_entry() to allow
    board-specific code to implement dynamic NS entry support
    for OPTEE booting.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I455bc0d7e39e3be66b6ab72a53b6dacaf3d2e503

--- arch/arm/mach-aspeed/ast2600/spl.c
+++ arch/arm/mach-aspeed/ast2600/spl.c
@@ -78,6 +78,71 @@ int spl_start_uboot(void)
 }
 #endif
 
+int spl_fit_images_get_uboot_entry(void *blob, uintptr_t *entry)
+{
+	int parent, node, ndepth;
+	const void *data;
+
+	if (!blob)
+		return -FDT_ERR_BADMAGIC;
+
+	parent = fdt_path_offset(blob, "/fit-images");
+	if (parent < 0)
+		return -FDT_ERR_NOTFOUND;
+
+	for (node = fdt_next_node(blob, parent, &ndepth);
+	     (node >= 0) && (ndepth > 0);
+	     node = fdt_next_node(blob, node, &ndepth)) {
+		if (ndepth != 1)
+			continue;
+
+		data = fdt_getprop(blob, node, FIT_OS_PROP, NULL);
+		if (!data)
+			continue;
+
+		if (genimg_get_os_id(data) == IH_OS_U_BOOT)
+			break;
+	};
+
+	if (node == -FDT_ERR_NOTFOUND)
+		return -FDT_ERR_NOTFOUND;
+
+	*entry = fdt_getprop_u32(blob, node, "entry-point");
+
+	if (*entry == FDT_ERROR)
+		*entry = fdt_getprop_u32(blob, node, "load-addr");
+
+	if (*entry == FDT_ERROR)
+		return -FDT_ERR_NOTFOUND;
+
+	return 0;
+}
+
+void spl_optee_entry(void *arg0, void *arg1, void *arg2, void *arg3)
+{
+	uintptr_t optee_ns_ep;
+	int rc;
+
+	rc = spl_fit_images_get_uboot_entry(arg2, &optee_ns_ep);
+	if (rc)
+		goto err_hang;
+
+	asm volatile(
+		"mov lr, %[ns_ep]\n\t"
+		"mov r0, %[a0]\n\t"
+		"mov r1, %[a1]\n\t"
+		"mov r2, %[a2]\n\t"
+		"bx %[a3]\n\t"
+		:
+		: [ns_ep]"r" (optee_ns_ep), [a0]"r" (arg0), [a1]"r" (arg1), [a2]"r" (arg2), [a3]"r" (arg3)
+		: "lr", "r0", "r1", "r2"
+	);
+
+err_hang:
+	debug("cannot find NS image entry for OPTEE\n");
+	hang();
+}
+
 void board_fit_image_post_process(const void *fit, int node, void **p_image, size_t *p_size)
 {
 	ulong s_ep;
