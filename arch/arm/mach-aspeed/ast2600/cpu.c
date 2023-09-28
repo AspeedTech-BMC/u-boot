72dbbdd8526787d796fee683dfa7a941c2c1f1e3
Ryan Chen <ryan_chen@aspeedtech.com>
2023-09-28 13:47:22 +0800

    ast2600: boot mode combine
    use boot strap to combine spi/emmc boot.
    
    Signed-off-by: Ryan Chen <ryan_chen@aspeedtech.com>
    Change-Id: Ib0cee27a5ebb63ef0d532184892ede581bbb3353

---
+++ arch/arm/mach-aspeed/ast2600/cpu.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) ASPEED Technology Inc.
+ */
+
+#include <common.h>
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <environment.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+enum env_location env_get_location(enum env_operation op, int prio)
+{
+	enum env_location env_loc = ENVL_UNKNOWN;
+
+	if (prio)
+		return env_loc;
+
+	if (readl(ASPEED_HW_STRAP1) & BIT(2))
+		env_loc =  ENVL_MMC;
+	else
+		env_loc =  ENVL_SPI_FLASH;
+
+	return env_loc;
+}
+
+int arch_misc_init(void)
+{
+	if (IS_ENABLED(CONFIG_ARCH_MISC_INIT)) {
+		if (readl(ASPEED_HW_STRAP1) & BIT(2))
+			env_set("bootcmd", EMMC_BOOTCOMMAND);
+		else
+			env_set("bootcmd", SPI_BOOTCOMMAND);
+	}
+
+	return 0;
+}
--- include/configs/evb_ast2600.h
+++ include/configs/evb_ast2600.h
@@ -25,14 +25,25 @@
 #define CONFIG_SPL_BSS_MAX_SIZE		0x00100000
 #define CONFIG_SPL_LOAD_FIT_ADDRESS	0x00010000
 
+#define CONFIG_SUPPORT_EMMC_BOOT
+
 /* Extra ENV for Boot Command */
 #define STR_HELPER(n)	#n
-#define STR(n)		STR_HELPER(n)
+#define _STR(n)		STR_HELPER(n)
+
+#define SPI_BOOTCOMMAND "run bootspi"
+#define EMMC_BOOTCOMMAND "run bootmmc"
 
 #undef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS	\
-	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
+	"loadaddr=" _STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
 	"bootspi=fdt addr 20160000 && fdt header get fitsize totalsize && cp.b 20160000 ${loadaddr} ${fitsize} && bootm; echo Error loading kernel FIT image\0"	\
+	"bootside=a\0"	\
+	"rootfs=rofs-a\0"	\
+	"setmmcargs=setenv bootargs ${bootargs} rootwait root=PARTLABEL=${rootfs}\0"	\
+	"boota=setenv bootpart 2; setenv rootfs rofs-a; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
+	"bootb=setenv bootpart 3; setenv rootfs rofs-b; run setmmcargs; ext4load mmc 0:${bootpart} ${loadaddr} fitImage && bootm; echo Error loading kernel FIT image\0"	\
+	"bootmmc=if test \"${bootside}\" = \"b\"; then run bootb; run boota; else run boota; run bootb; fi\0"	\
 	"verify=yes\0"	\
 	""
 
